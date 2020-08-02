e69ee276a101066a62feaed58112dab55d412cb5
Vincent-hide <hidehiro.okayama@gmail.com>
2020-08-01 20:50:50 -0400

    fragments added

--- src/main/java/spring/framework/eventorganizer/controllers/EventController.java
+++ src/main/java/spring/framework/eventorganizer/controllers/EventController.java
@@ -3,7 +3,9 @@ package spring.framework.eventorganizer.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Item 1");
-        events.add("Item 2");
-        events.add("Item 3");
-        events.add("Item 4");
         model.addAttribute("events", events); // passing an array list to events/index.html
         return "events/index";
     }
@@ -27,4 +26,11 @@ public class EventController {
     public String renderCreateEventForm() {
         return "events/create";
     }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String title) {
+        events.add(title);
+        return "redirect:/events";
+    }
+
 }
